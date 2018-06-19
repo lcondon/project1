c535e9bb363bb56dd8e266fd3175e927972283d5
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-06-18 20:45:37 -0400

    added form and algolia places

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,4 +6,11 @@
   
     }); // end of document ready
   })(jQuery); // end of jQuery name space
+
+  var placesAutocomplete = places({
+    container: document.querySelector("#cityInput"),
+    language: "en_US",
+    countries: "us",
+    aroundLatLngViaIP: false,
+  });
   

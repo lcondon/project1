72ed833a1ef22f620a5b7ef9f48e91ecaf906b3f
lcondon <lcondon@bennington.edu>
2018-06-20 19:53:49 -0400

    test for google api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,24 @@
-(function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
+(function ($) {
+  $(function () {
 
-  var placesAutocomplete = places({
-    container: document.querySelector("#cityInput"),
-    language: "en_US",
-    countries: "us",
-    aroundLatLngViaIP: false,
-  });
-  
-  
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+var placesAutocomplete = places({
+  container: document.querySelector("#cityInput"),
+  language: "en_US",
+  countries: "us",
+  aroundLatLngViaIP: false,
+});
+var queryUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Richmond%20Virginia&inputtype=textquery&fields=id,photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyA5Ag7VdkbFo4eRs7x383DpttV1xDr5uRk"
+
+$.ajax({
+  url: queryUrl,
+  method: 'GET'
+}).then(function (result) {
+  console.log(result);
+})
+

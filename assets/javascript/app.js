0e79296847f0f266588e42685785de0d17739c3d
lcondon <lcondon@bennington.edu>
2018-06-20 20:50:48 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 
-function ($) {
+(function ($) {
   $(function () {
 
     $('.sidenav').sidenav();
@@ -45,20 +45,5 @@ $("#searchBar").on("click", function (event) {
   console.log(zip);
   state = $("#form-state").val().trim();
   console.log(state);
-})
-
-var placesAutocomplete = places({
-  container: document.querySelector("#cityInput"),
-  language: "en_US",
-  countries: "us",
-  aroundLatLngViaIP: false,
-});
-var queryUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Richmond%20Virginia&inputtype=textquery&fields=id,photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyA5Ag7VdkbFo4eRs7x383DpttV1xDr5uRk"
-
-$.ajax({
-  url: queryUrl,
-  method: 'GET'
-}).then(function (result) {
-  console.log(result);
 })
 
